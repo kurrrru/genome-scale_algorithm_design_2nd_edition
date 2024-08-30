e24fabf0909e97836ceec5e356be5fc0dc6f32ca
kurrrru <kurrrru8850@gmail.com>
2024-08-30 17:10:15 +0900

    viterbi.cpp

---
+++ 07_hidden_markov_model/2_the_viterbi_algorithm/viterbi.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+double viterbi_dp(const std::vector<int> &seq, const std::vector<std::vector<double>> &trans, const std::vector<std::vector<double>> &emit, std::vector<std::vector<double>> &dp)
+{
+	int N = seq.size();
+	int H = trans.size();
+	dp.resize(N + 2, std::vector<double>(H, 0));
+	dp[0][0] = 1;
+	for (int i = 0; i < N; i++)
+	{
+		for (int h = 1; h < H - 1; h++)
+		{
+			double max_prob = 0;
+			for (int h_prev = 0; h_prev < H - 1; h_prev++)
+			{
+				max_prob = std::max(max_prob, dp[i][h_prev] * trans[h_prev][h]);
+			}
+			dp[i + 1][h] = max_prob * emit[h][seq[i]];
+		}
+	}
+	for (int h = 1; h < H - 1; h++)
+	{
+		dp[N + 1][H] = std::max(dp[N + 1][H], dp[N][h] * trans[h][H - 1]);
+	}
+	return dp[N + 1][H];
+}
+
+std::vector<int> viterbi_traceback(const std::vector<int> &seq, const std::vector<std::vector<double>> &trans, const std::vector<std::vector<double>> &emit, const std::vector<std::vector<double>> &dp)
+{
+	int N = seq.size();
+	int H = trans.size();
+	std::vector<int> path(N + 2, 0);
+	path[N + 1] = H;
+	for (int h = 1; h < H - 1; h++)
+	{
+		if (dp[N + 1][H] == dp[N][h] * trans[h][H - 1])
+		{
+			path[N] = h;
+			break;
+		}
+	}
+	for (int i = N - 1; i >= 0; i--)
+	{
+		for (int h = 1; h < H - 1; h++)
+		{
+			if (dp[i + 1][path[i + 1]] == dp[i][h] * trans[h][path[i + 1]] * emit[path[i + 1]][seq[i]])
+			{
+				path[i] = h;
+				break;
+			}
+		}
+	}
+	return path;
+}
+
+int main()
+{
+	std::vector<int> seq = {0, 1, 2, 1, 0};
+	std::vector<std::vector<double>> trans(
+		{
+			{0.0, 0.3, 0.7, 0.0},
+			{0.0, 0.5, 0.4, 0.1},
+			{0.0, 0.6, 0.3, 0.1},
+			{0.0, 0.0, 0.0, 0.0},
+		});
+	std::vector<std::vector<double>> emit(
+		{
+			{0.0, 0.0, 0.0},
+			{0.1, 0.4, 0.5},
+			{0.6, 0.3, 0.1},
+			{0.0, 0.0, 0.0},
+		});
+	std::vector<std::vector<double>> dp;
+	double prob = viterbi_dp(seq, trans, emit, dp);
+	std::cout << "Probability: " << prob << std::endl;
+	std::cout << "DP Table:" << std::endl;
+	for (int i = 0; i < dp.size(); i++)
+	{
+		for (int j = 0; j < dp[i].size(); j++)
+		{
+			std::cout << dp[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::vector<int> path = viterbi_traceback(seq, trans, emit, dp);
+	std::cout << "Path: ";
+	for (int i = 0; i < path.size(); i++)
+	{
+		std::cout << path[i] << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
